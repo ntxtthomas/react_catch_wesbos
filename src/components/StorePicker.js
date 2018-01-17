f8ca9964f760357277192bbfe856c6cc5cb3659a
ntxtthomas <apm.tthomas@gmail.com>
2018-01-16 22:14:49 -0600

    StorePicker1: parent element

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-
+// comments either go outside of the return...
   render() {
-    return <p>Hello World</p>
+    return (
+      <form className="store-collector">
+      {/* .. or inside of the parent in the return */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+      
+      )
   }
 }
 
